a9a8a30f723d09c1cf2c0b97eac56c3e878f63e9
Richard Walters <rwalters@digitalstirling.com>
2019-02-07 12:55:01 -0800

    Add Windows batch file for use in staging/running backend

---
+++ wdoit.cmd
@@ -0,0 +1,10 @@
+@set ROOT=%~dp0
+@set BUILD=%ROOT%build-vscode
+@set STAGE=%BUILD%\stage
+@IF NOT EXIST %STAGE% mkdir %STAGE%
+@copy %BUILD%\IronGloveBackend\IronGlove.exe %STAGE% > NUL
+@if "%~1"=="PePe" (
+    @%STAGE%\IronGlove
+) else (
+    call <nul %0 PePe
+)
