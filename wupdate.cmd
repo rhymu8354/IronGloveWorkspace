725507b27a073288082f3beb0ff9a2310af71e72
Richard Walters <rwalters@digitalstirling.com>
2019-02-13 13:46:30 -0800

    Include systems Lua scripts with the back-end

--- wupdate.cmd
+++ wupdate.cmd
@@ -2,10 +2,4 @@
 @set BUILD=%ROOT%build-vscode
 @set STAGE=%BUILD%\stage
 @IF NOT EXIST %STAGE% mkdir %STAGE%
-@copy %BUILD%\IronGloveBackend\IronGlove.exe %STAGE% > NUL
 @copy %ROOT%\IronGloveBackend\systems.lua %STAGE% > NUL
-@if "%~1"=="PePe" (
-    @%STAGE%\IronGlove
-) else (
-    call <nul %0 PePe
-)
