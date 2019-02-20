ecf93ed432f288c03a64cbb96de3f8247f4e4511
Richard Walters <rwalters@digitalstirling.com>
2019-02-20 11:52:51 -0800

    Pull in LuaJIT

--- wdoit.cmd
+++ wdoit.cmd
@@ -3,6 +3,7 @@
 @set STAGE=%BUILD%\stage
 @IF NOT EXIST %STAGE% mkdir %STAGE%
 @copy %BUILD%\IronGloveBackend\IronGlove.exe %STAGE% > NUL
+@copy %BUILD%\IronGloveBackend\lua51.dll %STAGE% > NUL
 @copy %ROOT%\IronGloveBackend\systems.lua %STAGE% > NUL
 @if "%~1"=="PePe" (
     @%STAGE%\IronGlove
